962f9e1ab45683a2141cee86e4a9fa641fcf622b
juseong kim <kjschocolate@gmail.com>
2022-07-11 23:31:43 +0900

    BOJ #10815 숫자 카드

---
+++ BOJ/10815/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Map<String, String> ht = new HashMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()){
+            ht.put(st.nextToken(), "1");
+        }
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()){
+            if(ht.containsKey(st.nextToken())){
+                System.out.print("1 ");
+            }else{
+                System.out.print("0 ");
+            }
+        }
+    }
+}
