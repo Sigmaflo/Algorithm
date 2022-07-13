5551bb0fb2ad9274961ff564a2879fd327f9f8a8
juseong kim <kjschocolate@gmail.com>
2022-07-13 22:27:08 +0900

    BOJ #10989 수 정렬하기 3 updated

--- BOJ/10989/Main.java
+++ BOJ/10989/Main.java
@@ -1,25 +1,24 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        short[] arr = new short[10001];
-        int n = Integer.parseInt(br.readLine());
-        for(int i = 0; i < n; i++){
-            short num = Short.parseShort(br.readLine());
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int[] arr = new int[10001];
+        for(int i = 1; i <= 10000; i++){
+            arr[i] = 0;
+        }
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++){
+            int num = Integer.parseInt(br.readLine());
             arr[num]++;
         }
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] != 0){
-                for(int s = 0; s < arr[i]; s++){
-                    bw.write(Integer.toString(i));
-                    bw.newLine();
-                }
+        for(int i = 1; i <= 10000; i++){
+            for(int j = 0; j < arr[i]; j++){
+                sb.append(i).append('\n');
             }
         }
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
